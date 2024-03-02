29f8e9795931884d369b1fde302b111fe6617ff7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-02 14:56:19 +0430

    adding some changes

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const btn = document.querySelector(".btn");
 const  content = document.querySelector(".content");
 
 const url = "https://api.chucknorris.io/jokes/random";
+
+btn.addEventListener("click", ()=> {
+    console.log("hello");
+})
